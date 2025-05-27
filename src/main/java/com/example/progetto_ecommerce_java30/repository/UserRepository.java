8eb380e31fde9e92761d5b21118b26b80204579d
djguruge <djguruge@gmail.com>
2025-05-27 18:12:25 +0000

    findByMail added

--- src/main/java/com/example/progetto_ecommerce_java30/repository/UserRepository.java
+++ src/main/java/com/example/progetto_ecommerce_java30/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.example.progetto_ecommerce_java30.repository;
 
 import com.example.progetto_ecommerce_java30.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
